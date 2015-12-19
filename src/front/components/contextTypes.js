d21d6d7cc9820c36b0efdd9ebd69bb5dd12b5da4
Dan Kaplun <dbkaplun@gmail.com>
2015-12-19 16:13:54 -0500

    style(contextTypes): stronger guarantee on app

--- src/front/components/contextTypes.js
+++ src/front/components/contextTypes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default {
-  app: React.PropTypes.object, // React.PropTypes.instanceOf(require('./App.jsx'))
+  app: React.PropTypes.shape({render: React.PropTypes.func.isRequired}), // React.PropTypes.instanceOf(require('./App.jsx'))
   fbRef: React.PropTypes.instanceOf(require('firebase')),
   history: React.PropTypes.shape({push: React.PropTypes.func.isRequired}),
   u: React.PropTypes.instanceOf(require('../../UserService'))
